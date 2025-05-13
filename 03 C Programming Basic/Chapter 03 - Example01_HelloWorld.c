7b36de11cbcd498bd3e812c9cb945f9869e3eafc
Leonardo <jleonardoqv@gmail.com>
2025-05-13 18:48:18 +0200

    Add programming basic

---
+++ 03 C Programming Basic/Chapter 03 - Example01_HelloWorld.c
@@ -0,0 +1,10 @@
+/* A first program in C */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   printf( "Hello World!\n" );
+
+   return 0; /* indicate that program ended successfully */
+} /* end function main */
